904721a3450bc9dd955fcc719457ae3d2207aa01
ulihg <ulihg@ULIHGU>
2024-04-09 17:58:35 +0800

    添加项目文件。

---
+++ MdPDF.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace MdPDF
+{
+    partial class MdPDF
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MdPDF));
+            this.panTop = new System.Windows.Forms.Panel();
+            this.FileOpen = new System.Windows.Forms.Label();
+            this.logo = new System.Windows.Forms.Label();
+            this.Min = new System.Windows.Forms.Label();
+            this.Max = new System.Windows.Forms.Label();
+            this.exit = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.menupage = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CurrentPage = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ViewerPdf = new PdfiumViewer.PdfViewer();
+            this.MenuePdf = new PdfiumViewer.PdfViewer();
+            this.rightPdf = new System.Windows.Forms.Label();
+            this.leftPdf = new System.Windows.Forms.Label();
+            this.panTop.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panTop
+            // 
+            this.panTop.BackColor = System.Drawing.Color.LightSlateGray;
+            this.panTop.Controls.Add(this.FileOpen);
+            this.panTop.Controls.Add(this.logo);
+            this.panTop.Controls.Add(this.Min);
+            this.panTop.Controls.Add(this.Max);
+            this.panTop.Controls.Add(this.exit);
+            this.panTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panTop.Location = new System.Drawing.Point(0, 0);
+            this.panTop.Margin = new System.Windows.Forms.Padding(6);
+            this.panTop.Name = "panTop";
+            this.panTop.Size = new System.Drawing.Size(2026, 58);
+            this.panTop.TabIndex = 6;
+            this.panTop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panTop_MouseDown);
+            // 
+            // FileOpen
+            // 
+            this.FileOpen.BackColor = System.Drawing.Color.Transparent;
+            this.FileOpen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.FileOpen.Dock = System.Windows.Forms.DockStyle.Left;
+            this.FileOpen.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold);
+            this.FileOpen.ForeColor = System.Drawing.Color.White;
+            this.FileOpen.Image = ((System.Drawing.Image)(resources.GetObject("FileOpen.Image")));
+            this.FileOpen.Location = new System.Drawing.Point(85, 0);
+            this.FileOpen.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.FileOpen.Name = "FileOpen";
+            this.FileOpen.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.FileOpen.Size = new System.Drawing.Size(69, 58);
+            this.FileOpen.TabIndex = 5;
+            this.FileOpen.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.FileOpen.MouseClick += new System.Windows.Forms.MouseEventHandler(this.FileOpen_MouseClick);
+            // 
+            // logo
+            // 
+            this.logo.BackColor = System.Drawing.Color.Transparent;
+            this.logo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.logo.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold);
+            this.logo.ForeColor = System.Drawing.Color.White;
+            this.logo.Image = global::MdPDF.Properties.Resources.Pdf;
+            this.logo.Location = new System.Drawing.Point(0, 0);
+            this.logo.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.logo.Name = "logo";
+            this.logo.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.logo.Size = new System.Drawing.Size(85, 58);
+            this.logo.TabIndex = 4;
+            this.logo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Min
+            // 
+            this.Min.BackColor = System.Drawing.Color.Transparent;
+            this.Min.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Min.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Min.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold);
+            this.Min.ForeColor = System.Drawing.Color.White;
+            this.Min.Image = global::MdPDF.Properties.Resources.最小化;
+            this.Min.Location = new System.Drawing.Point(1782, 0);
+            this.Min.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.Min.Name = "Min";
+            this.Min.Size = new System.Drawing.Size(85, 58);
+            this.Min.TabIndex = 3;
+            this.Min.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Min.Click += new System.EventHandler(this.Min_Click);
+            // 
+            // Max
+            // 
+            this.Max.BackColor = System.Drawing.Color.Transparent;
+            this.Max.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Max.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Max.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold);
+            this.Max.ForeColor = System.Drawing.Color.White;
+            this.Max.Image = global::MdPDF.Properties.Resources.最大化16;
+            this.Max.Location = new System.Drawing.Point(1867, 0);
+            this.Max.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.Max.Name = "Max";
+            this.Max.Size = new System.Drawing.Size(63, 58);
+            this.Max.TabIndex = 2;
+            this.Max.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Max.Click += new System.EventHandler(this.Max_Click);
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.Transparent;
+            this.exit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.exit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.exit.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold);
+            this.exit.ForeColor = System.Drawing.Color.White;
+            this.exit.Image = global::MdPDF.Properties.Resources.关闭16;
+            this.exit.Location = new System.Drawing.Point(1930, 0);
+            this.exit.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(96, 58);
+            this.exit.TabIndex = 1;
+            this.exit.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.panel2.Controls.Add(this.leftPdf);
+            this.panel2.Controls.Add(this.rightPdf);
+            this.panel2.Controls.Add(this.menupage);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.CurrentPage);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 1081);
+            this.panel2.Margin = new System.Windows.Forms.Padding(6);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(2026, 46);
+            this.panel2.TabIndex = 10;
+            // 
+            // menupage
+            // 
+            this.menupage.BackColor = System.Drawing.Color.Transparent;
+            this.menupage.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.menupage.Dock = System.Windows.Forms.DockStyle.Left;
+            this.menupage.Font = new System.Drawing.Font("楷体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.menupage.ForeColor = System.Drawing.Color.Red;
+            this.menupage.Location = new System.Drawing.Point(0, 0);
+            this.menupage.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.menupage.Name = "menupage";
+            this.menupage.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.menupage.Size = new System.Drawing.Size(320, 46);
+            this.menupage.TabIndex = 5;
+            this.menupage.Text = "页码：第00000页";
+            this.menupage.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Image = global::MdPDF.Properties.Resources.减号16;
+            this.label3.Location = new System.Drawing.Point(1732, 0);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 46);
+            this.label3.TabIndex = 3;
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // CurrentPage
+            // 
+            this.CurrentPage.BackColor = System.Drawing.Color.Transparent;
+            this.CurrentPage.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.CurrentPage.Dock = System.Windows.Forms.DockStyle.Right;
+            this.CurrentPage.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.CurrentPage.ForeColor = System.Drawing.Color.Red;
+            this.CurrentPage.Location = new System.Drawing.Point(1791, 0);
+            this.CurrentPage.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.CurrentPage.Name = "CurrentPage";
+            this.CurrentPage.Size = new System.Drawing.Size(139, 46);
+            this.CurrentPage.TabIndex = 2;
+            this.CurrentPage.Text = "100";
+            this.CurrentPage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label5.Dock = System.Windows.Forms.DockStyle.Right;
+            this.label5.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
+            this.label5.Location = new System.Drawing.Point(1930, 0);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 46);
+            this.label5.TabIndex = 1;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // ViewerPdf
+            // 
+            this.ViewerPdf.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ViewerPdf.Location = new System.Drawing.Point(409, 58);
+            this.ViewerPdf.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.ViewerPdf.Name = "ViewerPdf";
+            this.ViewerPdf.ShowBookmarks = false;
+            this.ViewerPdf.Size = new System.Drawing.Size(1617, 1023);
+            this.ViewerPdf.TabIndex = 16;
+            this.ViewerPdf.ZoomMode = PdfiumViewer.PdfViewerZoomMode.FitBest;
+            // 
+            // MenuePdf
+            // 
+            this.MenuePdf.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MenuePdf.Dock = System.Windows.Forms.DockStyle.Left;
+            this.MenuePdf.Location = new System.Drawing.Point(0, 58);
+            this.MenuePdf.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.MenuePdf.Name = "MenuePdf";
+            this.MenuePdf.ShowBookmarks = false;
+            this.MenuePdf.ShowToolbar = false;
+            this.MenuePdf.Size = new System.Drawing.Size(409, 1023);
+            this.MenuePdf.TabIndex = 12;
+            this.MenuePdf.ZoomMode = PdfiumViewer.PdfViewerZoomMode.FitBest;
+            // 
+            // rightPdf
+            // 
+            this.rightPdf.BackColor = System.Drawing.Color.Transparent;
+            this.rightPdf.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rightPdf.Dock = System.Windows.Forms.DockStyle.Right;
+            this.rightPdf.Font = new System.Drawing.Font("楷体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.rightPdf.ForeColor = System.Drawing.Color.White;
+            this.rightPdf.Image = ((System.Drawing.Image)(resources.GetObject("rightPdf.Image")));
+            this.rightPdf.Location = new System.Drawing.Point(1626, 0);
+            this.rightPdf.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.rightPdf.Name = "rightPdf";
+            this.rightPdf.Size = new System.Drawing.Size(106, 46);
+            this.rightPdf.TabIndex = 6;
+            this.rightPdf.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.rightPdf.Click += new System.EventHandler(this.rightPdf_Click);
+            // 
+            // leftPdf
+            // 
+            this.leftPdf.BackColor = System.Drawing.Color.Transparent;
+            this.leftPdf.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.leftPdf.Dock = System.Windows.Forms.DockStyle.Right;
+            this.leftPdf.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Bold);
+            this.leftPdf.ForeColor = System.Drawing.Color.White;
+            this.leftPdf.Image = ((System.Drawing.Image)(resources.GetObject("leftPdf.Image")));
+            this.leftPdf.Location = new System.Drawing.Point(1508, 0);
+            this.leftPdf.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.leftPdf.Name = "leftPdf";
+            this.leftPdf.Size = new System.Drawing.Size(118, 46);
+            this.leftPdf.TabIndex = 7;
+            this.leftPdf.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.leftPdf.Click += new System.EventHandler(this.leftPdf_Click);
+            // 
+            // MdPDF
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2026, 1127);
+            this.Controls.Add(this.ViewerPdf);
+            this.Controls.Add(this.MenuePdf);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panTop);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MdPDF";
+            this.Text = "PDF编辑器";
+            this.panTop.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panTop;
+        private System.Windows.Forms.Label Max;
+        private System.Windows.Forms.Label exit;
+        private System.Windows.Forms.Label Min;
+        private System.Windows.Forms.Label logo;
+        private System.Windows.Forms.Label FileOpen;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label menupage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label CurrentPage;
+        private System.Windows.Forms.Label label5;
+        private PdfiumViewer.PdfViewer ViewerPdf;
+        private PdfiumViewer.PdfViewer MenuePdf;
+        private System.Windows.Forms.Label rightPdf;
+        private System.Windows.Forms.Label leftPdf;
+    }
+}
+
---
+++ MdPDF.cs
@@ -0,0 +1,205 @@
+﻿using MdPDF.Properties;
+using PdfiumViewer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace MdPDF
+{
+    public partial class MdPDF : Form
+    {
+        private ContextMenuStrip contextMenuStrip;
+        public MdPDF()
+        {
+            InitializeComponent();           
+            //配置窗口类
+            FormBorderStyle = FormBorderStyle.None;//配置
+            StartPosition = FormStartPosition.CenterScreen;
+            // 创建ContextMenuStrip对象
+            contextMenuStrip = new ContextMenuStrip();
+
+            // 创建菜单项
+            ToolStripMenuItem menuItem1 = new ToolStripMenuItem("打开PDF");
+            ToolStripMenuItem menuItem2 = new ToolStripMenuItem("设置");
+            ToolStripMenuItem menuItem3 = new ToolStripMenuItem("退出");
+
+            // 添加菜单项到ContextMenuStrip
+            contextMenuStrip.Items.AddRange(new ToolStripItem[] { menuItem1, menuItem2, menuItem3 });
+
+            // 将ContextMenuStrip关联到表单，以便于弹出右键菜单
+            this.ContextMenuStrip = contextMenuStrip;
+
+            // 可选：为表单添加鼠标点击事件
+            this.MouseClick += new MouseEventHandler(FileOpen_MouseClick);
+        }
+        //窗口移动处理
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+        public const int WM_SYSCOMMAND = 0x0112;
+        public const int SC_MOVE = 0xF010;
+        public const int HTCAPTION = 0x0002;
+        private void exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Max_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+                //Image backImage = Resources.最大化;//这里图片调用的时资源库中添加好的图片  需要添加引用 using CalibrationAndMatching.Properties;
+                //btn_Max.BackgroundImage = backImage;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+                //Image backImage = Resources.还原;
+                //btn_Max.BackgroundImage = backImage;
+            }
+        }
+
+        private void Min_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void panTop_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                if (e.Clicks == 1)
+                {
+                    SendMessage(this.Handle, WM_SYSCOMMAND, SC_MOVE + HTCAPTION, 0);
+                }
+                else
+                {
+                    //MessageBox.Show("双周-0000");
+                    //SendMessage(this.Handle, WM_LBUTTONUP, 0, 0);
+                }
+            }
+        }
+
+        private void FileOpen_MouseClick(object sender, MouseEventArgs e)
+        {
+            // 如果是右键点击，显示弹出菜单
+            if (e.Button == MouseButtons.Left)
+            {
+                //contextMenuStrip.Show(this, e.Location);
+                try
+                {
+                    OpenFileDialog dialog = new OpenFileDialog
+                    {
+                        //设置打开标题、后缀
+                        Title = "请选择导入文件",
+                        Filter = "pdf文件|*.pdf"
+                    };
+                    if (dialog.ShowDialog() == DialogResult.OK)
+                    {
+                        if (dialog.FileName == "")
+                        {
+                            MessageBox.Show("空的文件，请重新选择！");
+                            return;
+                        }
+                        //_Path.Text = dialog.FileName;
+                        StartPdf(dialog.FileName);
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("空的文件，请重新选择！");
+                }
+            }
+        }
+
+        private void StartPdf(string pdfPath)
+        {
+            MenuePdf.Document?.Dispose();
+            MenuePdf.Document = OpenDocument(pdfPath);
+            MenuePdf.Renderer.Zoom = 0.8;
+            ViewerPdf.Document?.Dispose();
+            ViewerPdf.Document = MenuePdf.Document;
+            ViewerPdf.Renderer.Zoom = 1.5;
+            ViewerPdf.Renderer.DisplayRectangleChanged += Renderer_DisplayRectangleChanged;
+        }
+        private PdfiumViewer.IPdfDocument OpenDocument(string pdfPath)
+        {
+            try
+            {
+                return PdfiumViewer.PdfDocument.Load(this, pdfPath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+        /// <summary>
+        /// 右旋转90度页面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void rightPdf_Click(object sender, EventArgs e)
+        {            
+            // 获取当前页的索引
+            int currentPageIndex = ViewerPdf.Renderer.Page;
+            //MessageBox.Show("right当前页为：" + currentPageIndex);
+            // 获取PDF页面对象            
+                ViewerPdf.Document.RotatePage(currentPageIndex, PdfRotation.Rotate90);
+            // MessageBox.Show("right当前页为：" + currentPageIndex+ "numerXZ:" + numerXZ);
+            // 刷新PdfiumViewer控件显示
+            //ViewerPdf.Refresh();
+            //MenuePdf.Document.p
+        }
+        /// <summary>
+        /// 翻页矩形框变化时，更新当前页码
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Renderer_DisplayRectangleChanged(object sender, EventArgs e)
+        {
+            CurrentPage.Text = (ViewerPdf.Renderer.Page + 1).ToString();
+            try
+            {
+                menupage.Text = "页码：第" + (ViewerPdf.Renderer.Page + 1).ToString() + "/" + ViewerPdf.Document.PageCount + "页";
+            }
+            catch (Exception eer)
+            {
+
+            }
+        }
+        /// <summary>
+        /// 左旋转90度页面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void leftPdf_Click(object sender, EventArgs e)
+        {
+            //ViewerPdf.Renderer.RotateLeft();
+            //ViewerPdf.Renderer.;
+            // 获取当前页的索引
+            //var currentPageIndex = ViewerPdf.Renderer.Page;
+            var currentPageIndex = ViewerPdf.Renderer.Page;
+            MessageBox.Show("left当前页为：" + currentPageIndex);
+            // 获取PDF页面对象
+            //ViewerPdf.Document.RotatePage(currentPageIndex, PdfRotation.Rotate180);
+            
+            // 保存文档更改
+            //ViewerPdf.Document.Save();
+            // 刷新PdfiumViewer控件显示
+            ViewerPdf.Refresh();
+        }
+    }
+}
